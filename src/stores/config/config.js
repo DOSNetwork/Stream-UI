cada8fee1d2e38a941d9476a1c56176ea8468b6e
testrain <hsyjonny@gmail.com>
2021-03-02 21:45:02 +0800

    update config

--- src/stores/config/config.js
+++ src/stores/config/config.js
@@ -1,7 +1,7 @@
 const config = {
   provider: 'https://mainnet.infura.io/v3/211c5f35ed97405b86465c1d76502922',
   hecoTestProvider: 'https://http-testnet.hecochain.com',
-  CoingeckoStreamsManagerAddress: '0xe828E09d2BDE3bdB9bDa2e2A76437A15E6E86690',
+  CoingeckoStreamsManagerAddress: '0xeFC5cc56bf9a52acdb073c291382E815C78b5153',
   explorerUrl: 'https://testnet.hecoinfo.com/',
 };
 
